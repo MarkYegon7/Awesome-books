d3c0d73dcbdb05597d44519deabb78de919148e3
MarkYegon7 <markyegonk@gmail.com>
2023-08-23 23:51:51 +0300

    Add changes to bookCollection.js file

--- modules/bookCollection.js
+++ modules/bookCollection.js
@@ -1,4 +1,4 @@
-import displayBook from './display.js';
+const bookList = document.querySelector('#bookList');
 
 class BookCollection {
   constructor() {
@@ -16,14 +16,51 @@ class BookCollection {
       };
       this.bookArr.push(newBook);
       localStorage.setItem('booklist', JSON.stringify(this.bookArr));
-      displayBook();
+      this.displayBook();
     };
+
+    displayBook = () => {
+      bookList.innerHTML = '';
+      this.bookArr.forEach((book) => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        const titleParagraph = document.createElement('p');
+        titleParagraph.textContent = book.title;
+
+        const bookBy = document.createElement('span');
+        bookBy.textContent = 'by';
+        bookBy.classList.add('book-by');
+
+        titleParagraph.appendChild(bookBy);
+
+        const authorParagraph = document.createElement('p');
+        authorParagraph.textContent = book.author;
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.classList.add('deleteBtn');
+        removeButton.addEventListener('click', () => {
+          const indexToRemove = this.bookArr.findIndex((item) => item.id === book.id);
+          this.removeBookById(indexToRemove);
+        });
+
+        bookCard.appendChild(titleParagraph);
+        bookCard.appendChild(authorParagraph);
+        bookCard.appendChild(removeButton);
+        bookList.appendChild(bookCard);
+        if (bookList.innerHTML === '') {
+          bookList.classList.add('hide-book-list');
+        } else {
+          bookList.classList.remove('hide-book-list');
+        }
+      });
+    }
 
     removeBookById = (id) => {
       if (id !== -1) {
         this.bookArr.splice(id, 1);
         localStorage.setItem('booklist', JSON.stringify(this.bookArr));
-        displayBook();
+        this.displayBook();
       }
     };
 }
